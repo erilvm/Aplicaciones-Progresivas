18f861eb4fad3a175d24418b3ea3060798afdc8d
erilvm <erika.nemesio2002@gmail.com>
2024-01-24 00:38:51 -0600

    promesas6

--- Promesas.js
+++ Promesas.js
@@ -1,1 +1,27 @@
-//Ejercicio 5 de la imagen
+//Promesa 6
+
+const sumarLento = (numero) => {
+    return new Promise(
+        (resolve, reject) => {
+            setTimeout(() => {
+                 // resolve(numero + 1);
+               reject("Error")
+            }, 800);
+        }
+    )
+}
+
+const sumarRapido = (numero) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(numero + 1);
+            //reject("Error")
+        }, 800);
+    }
+    )
+}
+//Compite con todas las promesas y hace la que se ejecuta más rápido
+Promise.race(([ sumarLento(5),sumarRapido(6)]))
+.then((resultados) => {
+    console.log(resultados);
+}).catch(console.log);
