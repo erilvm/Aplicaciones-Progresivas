7cd4fcf61c03c394ce141c8e571d127f86b2fb95
erilvm <erika.nemesio2002@gmail.com>
2024-01-24 00:18:33 -0600

    promesa7

--- Promesas.js
+++ Promesas.js
@@ -1,26 +1,42 @@
-//Promesa 5
+//Promesa 7
 
-const sumarLento = (numero) => {
-    return new Promise(
-        (resolve, reject) => {
-            setTimeout(() => {
-                 resolve(numero + 1);
-                // reject("Error")
-            }, 800);
-        }
-    )
-}
+// Crea una función realizarOperacionesAsincronas que toma un array de funciones que devuelven promesas.
+//  La función debe ejecutar estas operaciones de forma secuencial, resolviendo cada promesa antes de pasar a la siguiente. 
+//  Si alguna promesa es rechazada, la función debe detenerse y rechazar con el motivo.
 
-const sumarRapido = (numero) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(numero + 1);
-        }, 300);
+async function realizarOperacionesAsincronas(arrayDeFunciones) {
+    try {
+      await Promise.all(arrayDeFunciones.map(funcion => funcion()));
+    } catch (error) {
+      throw error;
     }
-    )
-}
-// Si una falla fallan todas
-Promise.all(([ sumarRapido(6),sumarLento(5), true, 'Hola' ]))
-.then((resultados) => {
-    console.log(resultados);
-}).catch(console.log);
+  }
+  
+  const Promesa1 = () => new Promise(
+    (resolve) => setTimeout(() =>
+      resolve('Promesa 1 completa'), 900)
+  );
+//   const Promesa2 = () => new Promise(
+//     (resolve, reject) => setTimeout(() =>
+//       resolve('Promesa 2 completa'), 800)
+//   );
+  
+  const Promesa2 = () => new Promise(
+    (resolve, reject) => setTimeout(() =>
+      reject('Promesa 2 FALLIDA'), 800)
+  );
+  
+  const Promesa3 = () => new Promise(
+    (resolve) => setTimeout(() =>
+      resolve('Promesa 3 completa'), 700)
+  );
+  
+  const arrayDeFunciones = [Promesa1, Promesa2, Promesa3];
+  
+  realizarOperacionesAsincronas(arrayDeFunciones)
+    .then(() => console.log('Todas las operaciones completadas'))
+    .catch((error) => console.error('Error:', error));
+  
+
+//PRETIER
+//SONARLINE
