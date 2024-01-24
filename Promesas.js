5275d2d794183d9c1986175aaca5889d3cd1140f
erilvm <erika.nemesio2002@gmail.com>
2024-01-24 17:23:34 -0600

    fetch6

--- Promesas.js
+++ Promesas.js
@@ -1,1 +1,21 @@
-//Ejercicio 5 de la imagen
+//fetch 6
+
+fetch('https:fakestoreapi.com/products/1')
+.then(resp => {
+
+     resp.clone().json().then(data => {
+        console.log(data);
+     })
+     resp.clone().json().then(data => {
+        console.log(data);
+     })
+     resp.json().then(data => {
+        console.log(data);
+        
+     })
+    }
+)
+
+
+
+
