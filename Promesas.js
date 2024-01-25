7a9ff14aac759c4764dbe386631bd5266da3130d
erilvm <erika.nemesio2002@gmail.com>
2024-01-24 22:22:15 -0600

    fetch5

--- Promesas.js
+++ Promesas.js
@@ -1,1 +1,25 @@
-//Ejercicio 5 de la imagen
+// Almacena de manera local una imagen dentro de este Proyecto
+// en la carpeta img realiza una solicitud fetch a esa imagen y muestra la imagen en el navegador
+
+const rutaImagenLocal = './img/imagenn.jpeg';
+
+function mostrarImagen() {
+  fetch(rutaImagenLocal)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`No se pudo cargar la imagen. Estado: ${response.status}`);
+      }
+      return response.blob();
+    })
+    .then(blob => {
+      const imageUrl = URL.createObjectURL(blob);
+      const imagen = new Image();
+      imagen.src = imageUrl;
+
+      document.body.appendChild(imagen);
+    })
+    .catch(error => {
+      console.error('Error al cargar la imagen:', error);
+    });
+}
+mostrarImagen();
