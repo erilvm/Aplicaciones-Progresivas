e05ff8760f82dc5819974a12c962f741513ce891
erilvm <erika.nemesio2002@gmail.com>
2024-01-24 22:19:21 -0600

    fetch8

--- Promesas.js
+++ Promesas.js
@@ -1,18 +1,2 @@
-//fetch 7.1 = pagina de pokemon
-
-fetch('https:fakestoreapi.com/products/1')
-.then(resp => {
-     if (resp.ok) {
-          return resp.json()
-     } else {
-          throw new Error('NO existe el producto')
-     }
-     })
-     .then(console.log)
-     .catch(err => {
-      console.log('Error en la peticion')
-      console.log(err)
-     })
-    
 
 
