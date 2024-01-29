c9743c30265b38a76f4993135134af015f8569d1
erilvm <erika.nemesio2002@gmail.com>
2024-01-28 20:42:59 -0600

    fetch8

--- Promesas.js
+++ Promesas.js
@@ -1,1 +1,10 @@
-//html
+//Leer archivos html 
+fetch('not-found.html')
+.then(resp => resp.text())
+.then(html => {
+    document.querySelector('body').innerHTML = html
+})
+.catch(err => {
+    console.log('Error en la peticion')
+    console.log(err)
+})
